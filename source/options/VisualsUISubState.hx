65781f175a0aa63291f0a7641b97e66910acea29
Prohack101010 <miracberaber@gmail.com>
2024-07-05 02:35:07 +0300

    Bruh

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -25,6 +25,30 @@ import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
 
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import haxe.Json;
+import haxe.format.JsonParser;
+import Controls;
+
 using StringTools;
 
 class VisualsUISubState extends BaseOptionsMenu
